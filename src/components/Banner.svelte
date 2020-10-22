c1eceeff7590a6ab6c7d377761adccba81c2a6aa
sarah-w <sarah.wheaton@gmail.com>
2020-10-22 15:33:12 +1300

    css for banner

--- src/components/Banner.svelte
+++ src/components/Banner.svelte
@@ -9,31 +9,65 @@
   nav {
     padding:5px;
     background:#0b1736;
+    height:42px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items:center;
+  }
+
+  .leftitems{
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items:center;
+  }
+  .rightitems{
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items:center;
   }
   .avatar{
     border-radius: 50%;
-    height: 2rem;
+    height: 1.8rem;
   }
+
+  img{
+    height:1.5rem; 
+  }
+
   .logo{
-    height: 2rem;
+    padding: 0px 10px 0px 8px;
   }
-  a {
+
+  a.item {
     color:white;
-    font-size: 2rem;
+    font-size: 1.5rem;
     text-decoration: none;
+    padding: 0px 10px 0px 8px;
+    cursor: pointer;
   }
   
+  .usr{
+    padding: 0px 10px 0px 8px;
+  }
+
 </style>
 <nav>
-  <a href="/"><img src="svg/DV-logo-Horizontal-reverse.svg" class="logo"></a>
-  <a href="/user">User</a>
-  <a href="/admin">Admin</a>
-  <a href="/demo">Demo</a>
-  <span style="float:right">
-    {#await userPromise}
-      <img src="/svg/profile-icon.svg" alt="log out" class="avatar">
-    {:then user}
-      <img src={user.picture} on:click={logout} alt="log out" class="avatar">
-    {/await}
-  </span>
+  <div class="leftitems">
+    <a class="logo" href="/"><img src="svg/DV-logo-Horizontal-reverse.svg" ></a>
+    <a class = item href="/user">User</a>
+    <a class = item href="/admin">Admin</a>
+    <a class = item href="/demo">Demo</a>
+  </div>
+  <div class = rightitems>
+    <div class=usr>
+      {#await userPromise}
+        <img src="/svg/profile-icon.svg" alt="log out" class="avatar">
+      {:then user}
+        <img src={user.picture} on:click={logout} alt="log out" class="avatar">
+      {/await}
+    </div>
+  </div>
 </nav>
