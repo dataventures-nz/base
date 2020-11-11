dccb6f921ead1635b938767acf776e6fcbdb1a10
Blair Nilsson <blair.nilsson@gmail.com>
2020-11-11 15:37:08 +1300

    better demo is better

--- src/routes/demo/index.svelte
+++ src/routes/demo/index.svelte
@@ -2,7 +2,9 @@
   import {query} from '$components/api.mjs'
 </script>
 
-{#await query("population","noon_by_rto",[{$match:{RTO_code:1}},{$project:{_id:0,updated_from:0}}])}
+{#await query("population","noon_by_rto",[
+  {$match:{RTO_code:1, time:{$gte:{$date:"2020-10-01T12:00:00Z"}}}},
+  {$project:{_id:0,updated_from:0}}])}
   waiting....
 {:then results}
   {results.length}
