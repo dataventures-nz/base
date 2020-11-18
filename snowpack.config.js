951f87c057df6a99725747a7623dddde7c811a43
Sarah Wheaton <sarah.wheaton@gmail.com>
2020-11-19 11:05:26 +1300

    Update snowpack.config.js

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,4 @@
 // Consult https://www.snowpack.dev to learn about these options
 module.exports = {
-	plugins: [["@snowpack/plugin-build-script", { "cmd": "postcss", "input": [".css", ".pcss"], "output": [".css"] }]],
 	extends: '@sveltejs/snowpack-config'
 };
