6aca0636f2bdefb3c58fd7df3630261e182b6c1d
Blair Nilsson <blair.nilsson@gmail.com0>
2021-01-28 17:02:53 +1300

    looking at the right table

--- src/routes/demo/index.svelte
+++ src/routes/demo/index.svelte
@@ -1,9 +1,10 @@
 <script>
   import {query} from '$components/api.mjs'
-  let start = new Date(2020,10,1)
+  let start = new Date(2021,0,1)
+  console.log(start)
 </script>
 
-{#await query("population","noon_by_rto",[{$match:{RTO_code:1, time:{$gte:start}}}, {$project:{_id:0,updated_from:0}}])}
+{#await query("population","hourly_materialised",[{$match:{RTO_code:1, time:{$gte:start}}}, {$project:{_id:0,updated_from:0}}])}
   waiting....
 {:then results}
   {results.length}
