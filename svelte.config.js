4526566d8a5efadc18ac4262a532e4c1e67bc535
Blair Nilsson <blair.nilsson@gmail.com0>
2021-05-05 10:10:06 +1200

    update so you can see production builds

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-
+import node from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
         // Consult https://github.com/sveltejs/svelte-preprocess
@@ -8,9 +8,9 @@ const config = {
 
         kit: {
                 // hydrate the <div id="svelte"> element in src/app.html
-                target: '#svelte'
-        }
+                target: '#svelte',
+                adapter: node()
+              }
 };
 
 export default config;
-
