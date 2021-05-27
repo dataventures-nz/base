4d82683a198da678fbdfbe13d5f7967a1085d86f
Sarah-W <sarah.wheaton@gmail.com>
2021-05-28 10:28:13 +1200

    do not merge! halfway through datepicker refinements

---
+++ src/lib/datepicker/TimePicker.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let time
+  $:console.log({time})
+</script>
+
+<div style = "width:100%; display:flex; justify-content:center">
+  <input type="time" bind:value = {time}/>
+</div>
