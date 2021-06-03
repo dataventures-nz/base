8012adb6c797bc62a54b8b67fd9cea6185ef9ecd
Blair Nilsson <blair.nilsson@gmail.com0>
2021-06-03 13:16:34 +1200

    pushToLive script added.

---
+++ pushtolive.sh
@@ -0,0 +1,2 @@
+sudo docker build -t 343093121198.dkr.ecr.ap-southeast-2.amazonaws.com/dataventures-app-images .
+sudo docker push 343093121198.dkr.ecr.ap-southeast-2.amazonaws.com/dataventures-app-images
